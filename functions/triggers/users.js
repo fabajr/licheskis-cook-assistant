8c7b17fa57499729a16634aacc0555f065909e11
Fabiano Oliveira <fabiano95@msn.com>
2025-05-22 00:37:48 -0400

    feat: add user profile creation trigger on user registration

---
+++ functions/triggers/users.js
@@ -0,0 +1,21 @@
+// functions/triggers/users.js
+const { onUserCreated } = require('firebase-functions/v2/auth');
+const { db } = require('../firebaseAdmin');
+const { FieldValue } = require('firebase-admin/firestore');
+
+// toda vez que um usuário é criado no Auth, gera o documento em Firestore
+exports.createUserProfile = onUserCreated(
+  { region: 'us-central1' },
+  async (event) => {
+    const user = event.data;
+    await db.collection('users').doc(user.uid).set({
+      id: user.uid,
+      email: user.email,
+      display_name: user.displayName || '',
+      preferences: {},
+      hormonal_cycle: {},
+      created_at: FieldValue.serverTimestamp(),
+      updated_at: FieldValue.serverTimestamp(),
+    });
+  }
+);
